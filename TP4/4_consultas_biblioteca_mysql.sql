682a9161f6e5124eb2c623615d96f7975c1efcb9
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2026-05-31 19:31:34 -0300

    ejercicios a-c tp4 ej4

--- TP4/4_consultas_biblioteca_mysql.sql
+++ TP4/4_consultas_biblioteca_mysql.sql
@@ -1,14 +1,49 @@
 -- a) Aquellos ejemplares que jamás hayan sido retirados.
 
+select distinct isbn from ejemplar e
+left join prestamo p on p.idinventario=e.idinventario
+where p.idinventario  is null;
 
+select distinct isbn from ejemplar e
+where not exists (
+	select 1 from prestamo p
+	where p.idinventario=e.idinventario
+);
 
 -- b) Los libros pertenecientes a la categoría Marketing para los que haya habido pedidos insatisfechos.
 
-
+select p.isbn from pedidoinsatisfecho p
+inner join categorialibro cl on cl.isbn=p.isbn 
+inner join categoria cat on cat.idcategoria=cl.idcategoria
+where cat.categoria='Marketing';
 
 -- c) Los alumnos de Concordia que hayan retirado al menos dos libros durante el año 1999.
 
+start transaction;
 
+insert into usuario (dni, nombre, direccion, sexo, telefono, idlocalidad) 
+values (46151103,'Emmanuel Díaz','Perú 707','M','3455465213',3200);
+
+insert into alumno (dni,legajo,fingreso) 
+values (46151103,9999,'1980-01-01');
+
+insert into prestamo (fechaprestamo,idinventario,fechalimite,dni) values 
+  ('1999-03-20',1006,'1999-04-01',46151103),
+  ('1999-06-20',1007,'1999-07-01',46151103);
+
+select 
+  a.dni,
+  u.nombre,
+  count(p.dni) as cantidad
+from alumno a
+inner join usuario u on u.dni=a.dni
+inner join localidad l on u.idlocalidad=l.idlocalidad
+inner join prestamo p on p.dni=a.dni
+where l.localidad='CONCORDIA' and year(p.fechaprestamo)=1999
+group by a.dni, u.nombre
+having count(p.dni)>=2;
+
+rollback;
 
 -- d) Listar los departamentos de los cuales dependen todos aquellos investigadores que hayan retirado libros editados por 'Sudamericana'.
 
