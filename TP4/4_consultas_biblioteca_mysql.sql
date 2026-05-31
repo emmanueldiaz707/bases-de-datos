6129eee736ed78285a05d3194f9803521bab8054
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2026-05-31 19:30:50 -0300

    consignas tp4 ej4 biblioteca

---
+++ TP4/4_consultas_biblioteca_mysql.sql
@@ -0,0 +1,32 @@
+-- a) Aquellos ejemplares que jamás hayan sido retirados.
+
+
+
+-- b) Los libros pertenecientes a la categoría Marketing para los que haya habido pedidos insatisfechos.
+
+
+
+-- c) Los alumnos de Concordia que hayan retirado al menos dos libros durante el año 1999.
+
+
+
+-- d) Listar los departamentos de los cuales dependen todos aquellos investigadores que hayan retirado libros editados por 'Sudamericana'.
+
+
+
+-- e) El título de aquellos libros que hayan sido retirados tanto por docentes que dictan una determinada materia como por alumnos que cursan la misma.
+
+
+
+-- f) El nombre de los usuarios a los que se les ha vencido el plazo para devolver algún libro, y que con posterioridad a la fecha de vencimiento hayan retirado algún otro.
+
+
+
+-- g) Los docentes que dictan alguna materia que se dicte en todas las carreras.
+
+
+
+-- h) Aquellos libros para los que existe más de un ejemplar, tal que al menos dos de esos ejemplares se hayan encontrado prestados en forma simultánea en un determinado momento. 
+-- Para simplificar, considerar solamente aquellos préstamos en los que el libro ya haya sido devuelto.
+
+
